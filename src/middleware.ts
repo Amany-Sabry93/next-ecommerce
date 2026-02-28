c0d9d2ceea7fc23d2f5e944926a9391469f6cb89
Amany-Sabry93 <amanysabry1993mb@gmail.com>
2026-03-01 01:37:57 +0200

    first commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,5 +23,5 @@ export default async function middleware(request:NextRequest){
 }
 
  export const  config ={
-    matcher :['/cart','/my-orders','/login','/register']
+    matcher :['/cart','/allorders','/login','/register']
  }
