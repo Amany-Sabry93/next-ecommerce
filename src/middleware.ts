6ac0cc664510dc064af5f798649c6e282a0a5dfa
Amany-Sabry93 <amanysabry1993mb@gmail.com>
2026-03-01 02:20:44 +0200

    first commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,22 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(request:NextRequest){
-   const token= await getToken({req:request})
+   const token= await getToken({
+      req:request,
+      secret:process.env.AUTH_SECRET,
+      secureCookie:process.env.NODE_ENV==="production",
+      cookieName:process.env.NODE_ENV === "production"?
+      "__Secure-next-auth.session-token"
+      :"next-auth.session-token"
+
+   })
    const {pathname} = request.nextUrl
-   if(!token&& pathname=='/cart' || pathname== '/allorders'){
+   if(!token&&( pathname==='/cart' || pathname.startsWith( '/allorders'))){
       return NextResponse.redirect(new URL('/login',request.url))
 
    }
 
-   if(token && pathname=='/login' ||pathname=='/register'){
+   if(token && (pathname==='/login' ||pathname==='/register')){
 
      return NextResponse.redirect(new URL('/',request.url))
 
@@ -17,7 +25,7 @@ export default async function middleware(request:NextRequest){
 
 
 
-
+ return NextResponse.next()
 
  
 }
